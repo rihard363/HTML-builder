38491f541834b3256f6a861e9c02ba02bd0357a5
Aliaksei Savchenko <strag_king2@mail.ru>
2024-01-24 19:47:07 +0300

    fix: add cath and Kb task3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,15 +1,25 @@
 let fs = require('fs'),
   path = require('path');
 const wayPath = path.join(__dirname, 'secret-folder');
-fs.promises.readdir(wayPath, { withFileTypes: true }).then((files) => {
-  files.forEach((file) => {
-    if (file.isFile()) {
-      let filePath = path.join(wayPath, file.name);
-      let fileName = path.parse(filePath).name,
-        fileType = path.extname(filePath).replace('.', '');
-      fs.promises.stat(filePath).then((sample) => {
-        console.log(`${fileName} - ${fileType} - ${sample.size}`);
-      });
-    }
+fs.promises
+  .readdir(wayPath, { withFileTypes: true })
+  .then((files) => {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        let filePath = path.join(wayPath, file.name);
+        let fileName = path.parse(filePath).name,
+          fileType = path.extname(filePath).replace('.', '');
+        fs.promises
+          .stat(filePath)
+          .then((sample) => {
+            console.log(`${fileName} - ${fileType} - ${sample.size} Kb`);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-});
